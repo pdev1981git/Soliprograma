a10f8e42f9f2cf4fce99e8e71c19c86a10145840
Sergio <pdev1981git@gmail.com>
2023-03-01 07:29:57 -0300

    primer commit

---
+++ src/main/java/com/mycompany/soliprograma/Soliprograma.java
@@ -0,0 +1,22 @@
+package com.mycompany.soliprograma;
+
+
+public class Soliprograma {
+
+    public static void main(String[] args) {
+        
+        int numInicio = 5;
+        int numFinal = 14;
+        String numTot = "Hola";
+        
+        while(numInicio <= numFinal) {
+
+        System.out.println(numInicio);
+        numInicio++;}
+        System.out.println(numTot);
+
+    }
+        
+        
+    
+}
