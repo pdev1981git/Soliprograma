87ce3df10ca8b1b6b250c96202caccb3b3928131
Sergio <pdev1981git@gmail.com>
2023-03-01 18:27:31 -0300

    segundo commit

--- src/main/java/com/mycompany/soliprograma/Soliprograma.java
+++ src/main/java/com/mycompany/soliprograma/Soliprograma.java
@@ -5,7 +5,7 @@ public class Soliprograma {
 
     public static void main(String[] args) {
         
-        int numInicio = 5;
+       /* int numInicio = 5;
         int numFinal = 14;
         String numTot = "Hola";
         
@@ -13,7 +13,21 @@ public class Soliprograma {
 
         System.out.println(numInicio);
         numInicio++;}
-        System.out.println(numTot);
+        System.out.println(numTot);    */
+        
+        
+        String saludo = "hola que tal?";
+	System.out.println(saludo.toUpperCase());
+	System.out.println(saludo);
+	saludo = "hola y chau";
+	System.out.println(saludo);
+	saludo = saludo.toUpperCase();
+	System.out.println(saludo);
+	String [] saludoPartido1 = saludo.split(" ");
+	for (String palabra : saludoPartido1){
+		System.out.println(palabra);
+	}
+	System.out.println(saludo.length());
 
     }
         
